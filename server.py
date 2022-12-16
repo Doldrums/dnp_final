78bdbee624649b7afc496ba9188bd6bf6e5d133a
doldrums <doldrums.317@gmail.com>
2022-12-16 14:31:30 +0300

    12

--- server.py
+++ server.py
@@ -13,14 +13,15 @@ INTERNAL_DURATION = 5.0
 
 context = zmq.Context() 
 
-writer_client_socket = context.socket(zmq.REP)
+writer_client_socket = context.socket(zmq.SUB)
 writer_client_socket.bind(f'tcp://*:{writer_client_port}')
 writer_client_socket.RCVTIMEO = 100
+writer_client_socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
 reader_client_socket = context.socket(zmq.PUB)
 reader_client_socket.bind(f'tcp://*:{reader_client_port}')
 
-data = []
+data = {}
 time_diff = time.time()
 
 try: 
@@ -28,9 +29,9 @@ try:
         try:
             received_message = writer_client_socket.recv_string()
             print(f"Received request: {received_message}") 
-            name, message = map(received_message.split(':'))
+            name, message = received_message.split(':')
             if data.get(name) == None:
-                data[name] = 0
+                data[name] = 1
             else: 
                 data[name] = data.get(name) + 1
             reader_client_socket.send_string(received_message)
@@ -38,8 +39,10 @@ try:
                 continue 
         current = time.time()
         if current - time_diff  > INTERNAL_DURATION:
-            summary = f"""SUMMARY:
-                  {data}"""
+            summary = "SUMMARY:"
+            for i in sorted(data.keys()):
+                summary += f'\n\t{i}: {data[i]}'
+                data[i] = 0
             reader_client_socket.send_string(summary)
             time_diff = current
 except KeyboardInterrupt:
